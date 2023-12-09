486354412d95f0af27a1c83e48c62377dd73f650
Cheng-Tzu <80104408+Cheng-Tzu@users.noreply.github.com>
2023-12-09 19:50:47 +0800

    Update IG_foodie.py

--- IG_foodie.py
+++ IG_foodie.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# 套用所需要的套件
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,105 +10,63 @@ import time
 import urllib.request
 import datetime
 
+# ---------------------------------------------------------------------------------------------------------
 
-# In[155]:
-
-
-fddf = pd.read_excel('foodie.xlsx')
-fddf['sum'] =1
-
-
-# In[ ]:
-
-
-
-
-
-# In[165]:
-
-
+# 在此之前先使用多部(測試使用5部)手機在Instagram搜尋"台北食記" 並將前15個與foodie有關的帳號名稱列出來
+# 使用groupby計算每個帳號出現次數 大於等於4的帳號留下
+foodie_df = pd.read_excel('foodie.xlsx')
+foodie_df['sum'] =1
 fddf = fddf.groupby('NAME').sum()
 a = fddf['sum'] >= 4
 fddf = fddf[a].sort_values(by='sum', ascending=False)
-fddf.reset_index(inplace=True)
+fddf.reset_index()
 
-
-# In[166]:
-
-
-fddf
-
-
-# In[158]:
-
-
+# 將帳號名稱與Instagram前網址合併 產出list
 web=[]
 for i in range(len(fddf)):
     fd_nurl = 'https://www.instagram.com/' + fddf['NAME'][i]
     web.append(fd_nurl)
     print(fd_nurl)
 
-
-# In[ ]:
-
-
-
-
-
-# In[5]:
-
-
+# 載入webdrive
 browser = webdriver.Chrome()
 
-# ------ #台北美食 ------
+# 設定並進入目標網址(#台北美食)
 frist_url = 'https://www.instagram.com/explore/tags/%E5%8F%B0%E5%8C%97%E7%BE%8E%E9%A3%9F/'
-
 browser.get(frist_url) 
-
-
-# In[6]:
-
 
 time.sleep(2)
 browser.find_elements(by=By.CLASS_NAME, value='_acan._acap._acas')[0].click()
 time.sleep(2)
 
-# ------ 填入帳號與密碼 ------
+# 定位帳號、密碼框(開始前必須等到webdriver抓到帳號框的定位 以防網速不足出現error)
 WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, 'username')))
 username_input = browser.find_elements(by=By.NAME, value='username')[0]
 password_input = browser.find_elements(by=By.NAME, value='password')[0]
 print("inputing username and password...")
 
+# 輸入帳號密碼 需停頓 躲避防爬蟲機制+避免網速過慢出現error
 time.sleep(3)
-username_input.send_keys('instagram.scu.2022@gmail.com')
+username_input.send_keys('XXXXXXXX')
 print('username finish')
 time.sleep(2)
-password_input.send_keys('08170001')
+password_input.send_keys('YYYYYYYY')
 print('password finish')
 
-# ------ 登入 ------
+# 登入
 login_click = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[3]/button/div')
 time.sleep(3)
 login_click.click()
 print('login now')
 
-# ------ 不儲存登入資料 ------
+# 選擇不儲存登入資料(稍後再說)
 time.sleep(5)
-store_click = browser.find_element(by=By.XPATH, value='//*[@id="react-root"]/section/main/div/div/div/div/button')
+store_click = browser.find_element(by=By.CLASS_NAME, value='_ac8f')
 store_click.click()
-
 print("success!")
 
-
-# In[162]:
-
-
-web_df = pd.DataFrame(columns = ['name'])
-
-
-# In[163]:
-
-
+# 取得每個foodie帳號下數百篇文章的網址 並輸出成csv
+web_df = pd.DataFrame(columns = ['u_id'])
 t=0
 for t in range(len(web)):
     browser.get(web[t]) 
@@ -132,175 +85,20 @@ for t in range(len(web)):
     
         i+=1
         
-    name = browser.find_element(by=By.CLASS_NAME, value='_aacl._aacs._aact._aacx._aada')
+    post_user = browser.find_element(by=By.CLASS_NAME, value="_ap3a._aaco._aacw._aacx._aad7._aade")
     
     time.sleep(2)
     for url_p in post_url:
         url_np = 'https://www.instagram.com' + url_p
         web_df = web_df.append({
-            'name' : name.text,
+            'name' : post_user.text,
             'url' : url_np
         } , ignore_index=True)
         
     time.sleep(2)
     t+=1
 
-
-# In[164]:
-
-
-web_df
-
-
-# In[180]:
-
-
 web_df.to_csv('foodie3409.csv')
 
 
-# In[ ]:
-
-
-
-
-
-# In[117]:
-
-
-# ------ 存貼文網址 ------
-
-
-# In[179]:
-
-
-web_df.drop((3157:3200),axis=1)
-
-
-# In[42]:
-
-
-len(post_url)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[51]:
-
-
-href_df = pd.DataFrame(columns = ['id'])
-
-
-# In[52]:
-
-
-href_df['']
-
-
-# In[89]:
-
-
-name = browser.find_element(by=By.CLASS_NAME, value='_aacl._aacs._aact._aacx._aada')
-name.text
-
-
-# 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[76]:
-
-
-href_df = pd.DataFrame()
-for a in range(1):
-    browser.get(web[1])
-    time.sleep(3)
-    
-    for i in range(20):
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    
-        time.sleep(3)
-        for link in browser.find_elements_by_css_selector(".qi72231t.nu7423ey.n3hqoq4p.r86q59rh.b3qcqh3k.fq87ekyn.bdao358l.fsf7x5fv.rse6dlih.s5oniofx.m8h3af8h.l7ghb35v.kjdc1dyq.kmwttqpk.srn514ro.oxkhqvkx.rl78xhln.nch0832m.cr00lzj9.rn8ck1ys.s3jn8y49.icdlwmnq._a6hd"):
-            href = link.get_attribute('href')
-            if href.startswith('https://www.instagram.com/p/'):
-                href_df.append({
-                    'url' : href,
-                } , ignore_index=True)
-            else:
-                href_df.append({
-                    'url' : '',
-                } , ignore_index=True)
-    
-#     a+=1
-
-
-# In[77]:
-
-
-href_df
-
-
-# In[93]:
-
-
-aa = pd.DataFrame()
-aa['url'] = h
-aa.drop_duplicates()
-
-
-# In[73]:
-
-
-# u = browser.find_element(by=By.CLASS_NAME, value='qi72231t.nu7423ey.n3hqoq4p.r86q59rh.b3qcqh3k.fq87ekyn.bdao358l.fsf7x5fv.rse6dlih.s5oniofx.m8h3af8h.l7ghb35v.kjdc1dyq.kmwttqpk.srn514ro.oxkhqvkx.rl78xhln.nch0832m.cr00lzj9.rn8ck1ys.s3jn8y49.icdlwmnq._a6hd')
-u = browser.find_elements_by_css_selector(".qi72231t.nu7423ey.n3hqoq4p.r86q59rh.b3qcqh3k.fq87ekyn.bdao358l.fsf7x5fv.rse6dlih.s5oniofx.m8h3af8h.l7ghb35v.kjdc1dyq.kmwttqpk.srn514ro.oxkhqvkx.rl78xhln.nch0832m.cr00lzj9.rn8ck1ys.s3jn8y49.icdlwmnq._a6hd")
-
-
-# In[72]:
-
-
-u.get_attribute('href')
-
-
-# In[75]:
-
-
-for i in range(len(u)):
-    print(u[i].get_attribute('href'))
-
-
-# In[ ]:
-
-
-
-
+# 後續可依照IG_byhashtag.py中「爬取貼文」的部分操作
