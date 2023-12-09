c4bb3f397bd4ac70bea6c95649abea912d54f73c
Cheng-Tzu <80104408+Cheng-Tzu@users.noreply.github.com>
2023-12-09 18:46:34 +0800

    Update

--- IG_HOT 0812.py
+++ IG_HOT 0812.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# 套用所需要的套件
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,58 +10,51 @@ import time
 import urllib.request
 import datetime
 
+# ---------------------------------------------------------------------------------------------------------
 
-# In[2]:
-
-
+# 載入webdrive
 browser = webdriver.Chrome()
 
-# ------ #台北美食 ------
+# 設定並進入目標網址(#台北美食)
 frist_url = 'https://www.instagram.com/explore/tags/%E5%8F%B0%E5%8C%97%E7%BE%8E%E9%A3%9F/'
-
 browser.get(frist_url) 
-
-
-# In[3]:
-
 
 time.sleep(2)
 browser.find_elements(by=By.CLASS_NAME, value='_acan._acap._acas')[0].click()
 time.sleep(2)
 
-# ------ 填入帳號與密碼 ------
+# 定位帳號、密碼框(開始前必須等到webdriver抓到帳號框的定位 以防網速不足出現error)
 WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, 'username')))
 username_input = browser.find_elements(by=By.NAME, value='username')[0]
 password_input = browser.find_elements(by=By.NAME, value='password')[0]
 print("inputing username and password...")
 
+# 輸入帳號密碼 需停頓 躲避防爬蟲機制+避免網速過慢出現error
 time.sleep(3)
-username_input.send_keys('scu08170000')
+username_input.send_keys('XXXXXXXX')
 print('username finish')
 time.sleep(2)
-password_input.send_keys('08170000')
+password_input.send_keys('YYYYYYYY')
 print('password finish')
 
-# ------ 登入 ------
+# 登入
 login_click = browser.find_element(by=By.XPATH, value='//*[@id="loginForm"]/div/div[3]/button/div')
 time.sleep(3)
 login_click.click()
 print('login now')
 
-# ------ 不儲存登入資料 ------
+# 選擇不儲存登入資料(稍後再說)
 time.sleep(5)
-store_click = browser.find_element(by=By.XPATH, value='//*[@id="react-root"]/section/main/div/div/div/div/button')
+store_click = browser.find_element(by=By.CLASS_NAME, value='_ac8f')
 store_click.click()
-
 print("success!")
 
+# 回到指定網頁(#台北美食)
 browser.get(frist_url) 
 
-
-# In[4]:
-
-
-# ------ 抓貼文網址 ------
+# 抓貼文網址(格式為：'/p/.........../') 
+## 設定空List 使用迴圈將資料append進去 並輔以滑動頁面增加爬取到的貼文
+## range設定10是因為大約在9~11左右就會出現博弈、詐騙類混淆貼文
 post_url = []
 i=0
 for i in range(10):
@@ -79,52 +67,43 @@ for i in range(10):
     time.sleep(5) 
     
     i+=1
-
-
-# In[5]:
-
-
-# ------ 存貼文網址 ------
-url_df = pd.DataFrame(columns = ['date','url']) 
+    
+# 輸出正確可讀的網址
 webs = []
 for url_p in post_url:
     url_np = 'https://www.instagram.com' + url_p
     webs.append(url_np)
 
+# ---------------------------------------------------------------------------------------------------------
 
-# In[6]:
+# 開始爬取貼文
 
-
-# wurl = pd.DataFrame(columns = ['url']) 
-# wurl['url'] = webs
-# wurl.to_excel('C:/Users/Jessie_Cheng/畢專/eachday_url/0815.xlsx')
-
-
-# In[6]:
-
-
+## 定義四個DataFrame 貼文、標籤、讚數、圖片 並為每篇文章編號(a_id)
 content_df = pd.DataFrame(columns = ['a_id']) 
 ht_df = pd.DataFrame(columns = ['a_id'])
 like_df = pd.DataFrame(columns = ['a_id'])
 pic_df = pd.DataFrame(columns = ['a_id'])
 
-# ----------------------------------------------(post_start)----------------------------------------
+# 使用try except語法 避免網頁出錯(非code error)導致運行中斷 
 try:
     x = 0
+    # range(10)只處理10篇文 可依照所擁有的網址數量做調整
     for x in range(10):
+        
         url = webs[x] 
         browser.get(url)
     
         # 找到貼文的網頁元素(class)
         time.sleep(3)
-        post_user = browser.find_element(by=By.CLASS_NAME, value="oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl._acan._acao._acat._acaw._a6hd")
-        post_content_element = browser.find_element(by=By.CLASS_NAME, value="_aacl._aaco._aacu._aacx._aad7._aade")
-        post_hashtag = browser.find_elements(by=By.CLASS_NAME, value="oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl._aa9_._a6hd")
+        post_user = browser.find_element(by=By.CLASS_NAME, value="_ap3a._aaco._aacw._aacx._aad7._aade")
+        post_content_element = browser.find_element(by=By.CLASS_NAME, value="x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.xt0psk2.x1i0vuye.xvs91rp.xo1l8bm.x5n08af.x10wh9bi.x1wdrske.x8viiok.x18hxmgj")
+        post_hashtag = browser.find_elements(by=By.CLASS_NAME, value="x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._aa9_._a6hd")
 
     
 # ----------------------------------------------(content_df)----------------------------------------
+# 貼文df包含 文章編號、發文者ID、標註店家[有就抓(try) 沒有就補"None"(except)]、內文全文、網址、爬取時間(可有可無)
         try:
-            post_location = browser.find_element(by=By.CLASS_NAME, value="oajrlxb2.g5ia77u1.qu0x051f.esr5mh6w.e9989ue4.r7d6kgcz.rq0escxv.nhd2j8a9.nc684nl6.p7hjln8o.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.jb3vyjys.rz4wbd8a.qt6c0cv9.a8nywdso.i1ao9s8h.esuyzwwr.f1sip0of.lzcic4wl._aaqk._a6hd")
+            post_location = browser.find_element(by=By.CLASS_NAME, value="x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x5n08af.x9n4tj2._a6hd")
             content_df = content_df.append({
                 'a_id' : str(x+1),
                 'u_id' : post_user.text,
@@ -144,6 +123,7 @@ try:
             } , ignore_index=True)
         
 # ----------------------------------------------(ht_df)----------------------------------------
+# 標籤df包含 文章編號、標籤、給予int格式的1(後續會使用)
         for t in range(len(post_hashtag)):
             ht_df = ht_df.append({
                 'a_id' : str(x+1),
@@ -152,21 +132,26 @@ try:
             } , ignore_index=True)
 
 # ----------------------------------------------(pic_df)----------------------------------------
+# 照片df包含 文章編號、發文帳號、照片網址
         z = 0
         for z in range(10):
+            
             # 找到下一頁按鈕
-            but = browser.find_elements(by=By.CLASS_NAME, value='_aahi')
+            but = browser.find_elements(by=By.CLASS_NAME, value='_9zm2')
+
+            # 若沒有下一頁按鈕 直接儲存首張圖片的網址
             if but == []:
                 time.sleep(5)
                 soup = Soup(browser.page_source,"lxml")
-                post_img = soup.find_all(class_="_aagt")
-                p_url = (post_img[0].get('src'))
+                post_img = soup.find_all(class_="_aagv")[0]
+                tag = post_img.find('img')
+                p_url = tag.get('src')
                 pic_df = pic_df.append({
                     'a_id' : x+1,
                     'u_id' : post_user.text,
                     'pic' : p_url
                 } , ignore_index=True)
-    
+                
             else:
                 button = browser.find_elements(by=By.CLASS_NAME, value='_aahi')[0]
                 if button == None:
@@ -174,8 +159,9 @@ try:
                         break
                     else:
                         soup = Soup(browser.page_source,"lxml")
-                        post_img = soup.find_all(class_="_aagt")
-                        p_url = post_img[0].get('src')
+                        post_img = soup.find_all(class_="_aagv")[0]
+                        tag = post_img.find('img')
+                        p_url = tag.get('src')
                         pic_df = pic_df.append({
                             'a_id' : x+1,
                             'u_id' : post_user.text,
@@ -186,8 +172,9 @@ try:
                     button.click()
                     soup = Soup(browser.page_source,"lxml")
                     time.sleep(2)
-                    post_img = soup.find_all(class_="_aagt")
-                    p_url = post_img[0].get('src')
+                    post_img = soup.find_all(class_="_aagv")[0]
+                    tag = post_img.find('img')
+                    p_url = tag.get('src')
                     pic_df = pic_df.append({
                         'a_id' : x+1,
                         'u_id' : post_user.text,
@@ -195,36 +182,36 @@ try:
                     } , ignore_index=True)
             z ++ 1
 
+        # 由於有出現下一頁按鈕會不斷翻頁 為避免最後一張照片沒有爬取到 需要再補一次code 後續資料處理可刪除重複行
         time.sleep(5)
-        post_img = soup.find_all(class_="_aagt")
-        p_url = post_img[1].get('src')
+        post_img = soup.find_all(class_="_aagv")
+        tag = post_img[0].find('img')
         pic_df = pic_df.append({
             'a_id' : x+1,
             'u_id' : post_user.text,
             'pic' : p_url,
         } , ignore_index=True)
 # ----------------------------------------------(like_df)----------------------------------------
+# 讚數df包含 文章編號、讚數(部分貼文不會顯示總讚數 改為進入按讚名單頁面滑動網頁後以帳號數量作為讚數儲存)
         time.sleep(5)
         try:
-            post_likes = browser.find_element(by=By.CLASS_NAME, value='_aacl._aaco._aacw._aacx._aada._aade')
-            post_likes = post_likes.text.split(' ')[0]    
-            if post_likes == '其他人':
+            post_likes = browser.find_elements(by=By.CLASS_NAME, value='x193iq5w.xeuugli.x1fj9vlw.x13faqbe.x1vvkbs.xt0psk2.x1i0vuye.xvs91rp.x1s688f.x5n08af.x10wh9bi.x1wdrske.x8viiok.x18hxmgj')
+            if len(post_likes) == 1':
                 browser.get(url + 'liked_by/')
                 for lk in range(20):
                     browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
                     time.sleep(2)
-                num_like = browser.find_elements(by=By.CLASS_NAME, value='_ab8w._ab94._ab97._ab9f._ab9k._ab9p._ab9-._aba8')
+                num_like = browser.find_elements(by=By.CLASS_NAME, value='x1dm5mii.x16mil14.xiojian.x1yutycm.x1lliihq.x193iq5w.xh8yej3')
                 nlike = len(num_like)
                 time.sleep(2)
                 browser.get(url)
                 time.sleep(2)
-                post_content_element = browser.find_element(by=By.CLASS_NAME, value="_aacl._aaco._aacu._aacx._aad7._aade")
                 like_df = like_df.append({
                     'a_id' : str(x+1),
                     'sum_of_like' : nlike
                 } , ignore_index=True)
             else:
-                num_like = int(post_likes)
+                num_like = post_likes[0].text.replace('個讚','')
                 like_df = like_df.append({
                     'a_id' : str(x+1),
                     'sum_of_like' : num_like
@@ -237,133 +224,35 @@ try:
 
         time.sleep(2)    
         x ++ 1
-    
+
+    # 上方code完成後顯示"Post Finish"
     print('Post Finish')
     
 except:
+    # 若一開始就失敗則會顯示"Post Not Finish"
     print('Post Not Finish')
     pass
 
+# ---------------------------------------------------------------------------------------------------------
 
-# In[17]:
-
-
-content_df
-
-
-# In[18]:
-
-
-ht_df
-
-
-# In[19]:
-
-
+# 處理標籤 > 使用groupby以文章標籤為主 計算標籤數量 
 ht_sum_df = ht_df.groupby('a_id').sum()
 ht_sum_df
 
-
-# In[20]:
-
-
-like_df
-
-
-# In[21]:
-
-
+# 處理照片 > 將含時分秒的日期時間格式改為純日期格式 > 使用drop_duplicates刪除重複欄位
 pic_df['date'] = datetime.date.today().strftime("%Y-%m-%d")
+pic_df = pic_df.drop_duplicates()
 pic_df.reset_index(inplace=True)
 pic_df = pic_df.drop('index',axis=1)
 pic_df
 
 
-# ## export CSV
-
-# In[22]:
-
-
+# 合併貼文、標籤數、讚數三個列數相同的dataframe並輸出(含中文檔案還是建議輸出excel檔 csv配上encoding有時還是會有亂碼出現)
 article_df = pd.merge(content_df,ht_sum_df, on='a_id')
 article_df = pd.merge(article_df,like_df, on='a_id')
 article_df['a_id'] = article_df['a_id'].astype(int)
 article_df = article_df[['a_id','u_id','content','url','sum_of_like','sum_of_hashtag','createddatetime']]
-article_df
+article_df.to_excel('IG_main_df.xlsx')
 
-
-# In[23]:
-
-
-hashtag_df = ht_df.drop('sum_of_hashtag',axis=1)
-hashtag_df['a_id'] = hashtag_df['a_id'].astype(int)
-hashtag_df
-
-
-# In[24]:
-
-
-pic_df
-
-
-# In[15]:
-
-
-# date = datetime.date.today().strftime("%Y-%m-%d").replace('-','')[4:]
-# article_df.to_csv('article'+ date +'.csv')
-# hashtag_df.to_csv('hashtag'+ date +'.csv')
-# pic_df.to_csv('pic'+ date +'.csv')
-
-
-# ## download pic
-
-# In[16]:
-
-
-# i=0
-# for i in range(44):
-#     urllib.request.urlretrieve(pic_df['pic'][i], pic_df['u_id'][i] + '_' + pic_df['date'][i] + '_' + str(i) + '.jpg')
-#     print("download successful")
-#     time.sleep(2)
-#     i+=1
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-d=0
-for d in range(2):
-
-
-time.sleep(5)
-
-
-
-
-
-d+=1    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+# 輸出照片dataframe
+pic_df.to_csv('IG_pic_url.csv')
